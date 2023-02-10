674f9bb95542917ebe5477e04eda092b24d3a716
Alejandro <alexsotog82@gmail.com>
2023-02-10 13:48:04 -0600

    :construction: begin project with videojs and react, add maarks on the video
    a lab for integrate video js and put marks on the video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import { Player } from "./Player";
+import "video.js/dist/video-js.min.css";
+
+export default function App() {
+  const Myplayer = {};
+
+  const [state, setstate] = useState({
+    video: {
+      src: "http://vjs.zencdn.net/v/oceans.mp4",
+      poster:
+          "./image.jpg"
+    }
+  });
+
+  function onPlayerReady(player) {
+    console.log("Player is ready: ", player);
+    player = Myplayer;
+  }
+
+  function onVideoPlay(duration) {
+    console.log("Video played at: ", duration);
+  }
+
+  function onVideoPause(duration) {
+    console.log("Video paused at: ", duration);
+  }
+
+  function onVideoTimeUpdate(duration) {
+    console.log("Time updated: ", duration);
+  }
+
+  function onVideoSeeking(duration) {
+    console.log("Video seeking: ", duration);
+
+  }
+
+  function onVideoSeeked(from, to) {
+    console.log(`Video seeked from ${from} to ${to}`);
+  }
+
+  function onVideoEnd() {
+    console.log("Video ended");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Player
+            controls={true}
+            src={state.video.src}
+            poster={state.video.poster}
+            width="650"
+            height="420"
+            // onReady={onPlayerReady}
+            // onPlay={onVideoPlay}
+            onPause={onVideoPause}
+            // onTimeUpdate={onVideoTimeUpdate}
+            onSeeking={onVideoSeeking}
+            onSeeked={onVideoSeeked}
+            onEnd={onVideoEnd}
+        />
+      </div>
   );
 }
-
-export default App;
